5cc9725692da6e29f731b3ee5bcc93c81a87a4a2
Paul <paul@paul-HP-Pavilion-dv6700-Notebook-PC.(none)>
2012-02-25 19:39:04 -0500

    First Commit

---
+++ src/com/whatsthehapps/WelcomePage.java
@@ -0,0 +1,49 @@
+package com.whatsthehapps;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class WelcomePage extends Activity implements OnClickListener{
+
+	EditText etUsername, etPassword;
+	Button bLogin, bRegister;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		initialize();
+	}
+
+	private void initialize() {
+		// TODO Auto-generated method stub
+		etUsername = (EditText) findViewById(R.id.etUsername);
+		etPassword = (EditText) findViewById(R.id.etPassword);
+		bLogin = (Button) findViewById(R.id.btnLogin);
+		bRegister = (Button) findViewById(R.id.btnRegister);
+		bLogin.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId()) {
+		case R.id.btnLogin:
+			
+			break;
+			
+		case R.id.btnRegister:
+		
+		break;
+		}
+	}
+
+	
+	
+	
+}
